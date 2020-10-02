3b66266bc3e44756a7d548bba9ca19954a8a0e3d
OSAMA ANSARI <mohdbilal16@gmail.com>
2020-10-02 09:57:25 +0530

    header commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB342B4j8zcll3h1NS0EdeckbsbCpLD2gI",
+    authDomain: "clone-2b01a.firebaseapp.com",
+    databaseURL: "https://clone-2b01a.firebaseio.com",
+    projectId: "clone-2b01a",
+    storageBucket: "clone-2b01a.appspot.com",
+    messagingSenderId: "500548388844",
+    appId: "1:500548388844:web:786f4b5e2fbd16849be2c9",
+    measurementId: "G-VY1D463TYD"
+  };
